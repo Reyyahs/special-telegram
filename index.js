b1d8193d05fcb82e86b9abf8973d4f1537b06e7d
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-06 22:32:39 -0400

    Added inquirer prompt for user input to index.js and changed inquirer to ver. 8.2.4

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+function getUserInput() {
+    const questions = [
+        {
+            type: 'input',
+            message: 'Please provide the project title:',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Describe your project:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions:',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Specify contribution guidelines:',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Provide testing instructions:',
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: 'Choose project license type:',
+            choices: ['MIT License'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'Enter your GitHub username:',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'Enter your email address:',
+            name: 'email',
+        },
+    ];
+
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Initialize the application
+function init() {
+    getUserInput();
+}
 
-// Function call to initialize app
+// Start the application
 init();
