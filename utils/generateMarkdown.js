23c9216362136d6a255aa7939cff9bb15fcdf3d8
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-14 04:31:05 -0400

    created a function the returns the license section of the README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,12 +10,34 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseUrls = {
+    "Apache License 2.0": "apache-2.0",
+    "Boost Software License 1.0": "bsl-1.0",
+    "GNU AGPLv3": "agpl-3.0",
+    "GNU GPLv3": "gpl-3.0",
+    "GNU LGPLv3": "lgpl-3.0",
+    "MIT": "mit",
+    "Mozilla Public License 2.0": "mpl-2.0",
+    "Unlicense": "unlicense"
+  };
+
+  if (license === "None" || !licenseUrls.hasOwnProperty(license)) {
+    return "";
+  } else {
+    return `https://choosealicense.com/licenses/${licenseUrls[license]}`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `This project is licensed under the ${license}.`;
+  }
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
